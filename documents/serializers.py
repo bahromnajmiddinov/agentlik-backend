f8a8ecf24ff7fddc69676cd06fe196032c4ed6c1
bahromnajmiddinov <najmiddinovbahrom402@gmail.com>
2024-10-25 09:50:39 +0500

    Document category serializer updated

--- documents/serializers.py
+++ documents/serializers.py
@@ -3,13 +3,16 @@ from rest_framework import serializers
 from .models import Document, DocumentCategory
 
 
-class DocumentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Document
-        fields = '__all__'
-
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = DocumentCategory
         fields = '__all__'
+        
+
+class DocumentSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Document
+        fields = '__all__'
+
