304d0b9b849a09aba62922d5e2cf5dd0e724f1d3
bahromnajmiddinov <najmiddinovbahrom402@gmail.com>
2024-10-25 09:00:21 +0500

    Updated

--- config/settings.py
+++ config/settings.py
@@ -151,13 +151,13 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Whitenoice
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+# STORAGES = {
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Cors
 CORS_ALLOW_ALL_ORIGINS = True
