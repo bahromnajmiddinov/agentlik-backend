95a56941a1af3f652d56e8cd892a0261e654e53e
Bahrom <najmiddinovbahrom402@gmail.com>
2024-11-05 11:20:45 +0500

    Polls updated

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
-from .models import Poll, Choice
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from drf_spectacular.types import OpenApiTypes
+
+from .models import Poll
 from .serializers import PollSerializer
 
 
@@ -16,7 +20,21 @@ class PollDetailView(RetrieveAPIView):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
     lookup_field = 'id'
+    permission_classes = [AllowAny]
 
+    @extend_schema(
+        summary="Add click to poll",
+        description="This endpoint to add new clicks for poll.",
+        responses={200: PollSerializer},
+        parameters=[
+            OpenApiParameter(
+                name="choice_id",
+                type=OpenApiTypes.INT,
+                location=OpenApiParameter.QUERY,
+                description='Choice ID of poll.',
+            )
+        ]
+    )
     def put(self, request, id):
         choice_id = request.GET.get('choice_id', 0)
 
@@ -26,4 +44,4 @@ class PollDetailView(RetrieveAPIView):
         choice.clicks += 1
         choice.save()
 
-        return Response(status=200)
+        return Response(status=200, data=PollSerializer(poll).data)
